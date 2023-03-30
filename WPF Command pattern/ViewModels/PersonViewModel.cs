2930c8fef3f40e3dee479e61c3cea241afb1e09f
stepguest <stepguest@STHQ012E-03>
2023-03-30 11:47:29 +0400

    Add project files.

---
+++ WPF Command pattern/ViewModels/PersonViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_Command_pattern.Model;
+
+namespace WPF_Command_pattern.ViewModels
+{
+    internal class PersonViewModel : INotifyPropertyChanged
+    {
+
+        private Person _person;
+
+        public Person Person
+        {
+            get { return _person; }
+            set
+            {
+                _person = value;
+                OnPropertyChanged(nameof(Person));
+            }
+        }
+
+        public PersonViewModel() => Person = new Person();  
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+       
+    }
+}
